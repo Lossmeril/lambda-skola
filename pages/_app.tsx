99d52650bc4b26264d6e8c5c6d23987edede54dd
Karel Koudelka <k.koudelka01@gmail.com>
2023-06-22 16:30:51 +0200

    fix: app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,11 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import theme from "@/styles/theme";
-import Navbar from "@components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Navbar />
+      {/* @ts-ignore */}
       <Component {...pageProps} />
     </ChakraProvider>
   );
